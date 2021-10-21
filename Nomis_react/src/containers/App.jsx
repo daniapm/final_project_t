23a75d876db0d162825a7c883b8abf3e1aa48635
Carolina <carolinaramon239578@correo.itm.edu.co>
2021-10-21 10:10:47 -0500

    React router config

--- Nomis_react/src/containers/App.jsx
+++ Nomis_react/src/containers/App.jsx
@@ -1,17 +1,31 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
 import Border from '../components/Border/Border'
 import DropArea from '../components/DropArea/DropArea'
+import React  from 'react';
 import './App.css'
 
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Border />
-      <Footer />
-    </div>
+    
+    <Router>
+      <div className="App">
+          <Header />
+          <Border />
+          <Footer />
+        <Switch>
+          <Route path="/DropArea">
+            <DropArea />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
     );
 }
 

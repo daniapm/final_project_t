62204da30172a3f9e6515bc79c7977802c232ce1
daniapm <daniajibellis@hotmail.com>
2021-10-21 11:50:03 -0500

    #9-edit app.jsx file

--- Nomis_react/src/containers/App.jsx
+++ Nomis_react/src/containers/App.jsx
@@ -3,7 +3,6 @@ import Footer from '../components/layout/Footer/Footer'
 import DropArea from '../components/DropArea/DropArea'
 import MainContainer from '../components/layout/MainContainer/MainContainer'
 import MainContent from '../components/layout/MainContent/MainContent'
-import "/home/daniapm/tributi/final_project_tributi/Nomis_react/src/assets/static/logo.jpg"
 import StepComponent from '../components/Steps/Steps'
 
 
